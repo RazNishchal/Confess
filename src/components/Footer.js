b4f0d484342702e890b69a70e6179ea5cf92dd07
Nishchal Raj Subedi <111012172+RazNishchal@users.noreply.github.com>
2026-01-15 20:58:52 +0545

    Update copyright year in Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import "../assets/css/Footer.css";
 const Footer = () => {
   return (
     <div className="footer">
-      Copyright &copy; 2022 - {new Date().getFullYear()} | All rights reserved
+      Copyright &copy; 2025 - {new Date().getFullYear()} | All rights reserved
       by Confess Note Inc.
     </div>
   );
